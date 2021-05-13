b7039c6bb5e03741092490e3cf9dd488ccffca40
hari-hech-kay <48200258+hari-hech-kay@users.noreply.github.com>
2021-05-13 11:44:42 +0530

    initial commit

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,82 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import com.example.demo.repository.EmployeeRepository;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/")
+@CrossOrigin(origins="http://localhost:4200")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+	
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId)
+		throws ResourceNotFoundException {
+			Employee employee = employeeRepository.findById(employeeId).orElseThrow(()-> new ResourceNotFoundException("Employee not found for this id: " + employeeId));
+			return ResponseEntity.ok().body(employee);
+			}
+	
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long employeeId,  @Validated @RequestBody Employee employeeDetails) throws ResourceNotFoundException {
+		Employee employee = employeeRepository.findById(employeeId).orElseThrow(()-> new ResourceNotFoundException("Employee not found for this id: " + employeeId));
+		
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmail(employeeDetails.getEmail());
+		employee.setDepartment(employeeDetails.getDepartment());
+		employee.setDesignation(employeeDetails.getDesignation());
+		employee.setSalary(employeeDetails.getSalary());	
+		
+		final Employee updatedEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok().body(updatedEmployee);
+		
+	}
+	
+	 @DeleteMapping("/employees/{id}")
+	    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
+	         throws ResourceNotFoundException {
+	        Employee employee = employeeRepository.findById(employeeId)
+	       .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+
+	        employeeRepository.delete(employee);
+	        Map<String, Boolean> response = new HashMap<>();
+	        response.put("deleted", true);
+	        return response;
+}
+}
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,106 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+	
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
+	
+	@Column(name = "salary")
+	private float salary;
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+	
+	@Column(name = "department")
+	private String department;
+	
+	@Column(name = "designation")
+	private String designation;
+	
+	public Employee() {}
+
+	public Employee(long id, String firstName, String lastName, float salary, String email, String department,
+			String designation) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+		this.email = email;
+		this.department = department;
+		this.designation = designation;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
+	
+	
+}
